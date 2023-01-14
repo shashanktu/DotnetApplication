2c787a2ae10ee041664fff09cd767f30769c6607
Raj Jammana <Chandrasekhar.Siddabattula@valuemomentum.com>
2023-01-14 19:45:36 +0530

    adding dot net source code

---
+++ AzDevChaosTesting.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzDevChaosTesting.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
