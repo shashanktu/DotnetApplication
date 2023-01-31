3cce670a2f3e785bcd24be285dcbf011badfe7b8
Raj Jammana <Chandrasekhar.Siddabattula@valuemomentum.com>
2023-01-31 20:08:24 +0530

    aks updated in main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -14,3 +14,32 @@ resource "azurerm_resource_group" "dev" {
   name     = "DCS_assets_terraform"
   location = "East US"
 }
+resource "azurerm_kubernetes_cluster" "k8s" {
+  location            = "eastus"
+  name                = "k8scluster"
+  resource_group_name = dev.name
+  dns_prefix          = "k8scluster98794"
+  tags                = {
+    Environment = "Development"
+  }
+
+  default_node_pool {
+    name       = "agentpool"
+    vm_size    = "Standard_D2_v2"
+    node_count = "2"
+  }
+  linux_profile {
+    admin_username = "admin"
+
+    ssh_key {
+      key_data = file("~/.ssh/id_rsa.pub")
+    }
+  }
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+}
