49af36289a0c3c24700402f16559cf0217cad990
Raj Jammana <Chandrasekhar.Siddabattula@valuemomentum.com>
2023-01-31 17:41:17 +0530

    adding

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,13 +1,13 @@
-#Create Storage Account
 terraform {
-  required_providers {  
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.11, < 4.0"
-    }
-  }
-}
-provider "azurerm" {
+  required_version = ">= 0.11" 
+ backend "azurerm" {
+  storage_account_name = "__terraformstorageaccount__"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+	access_key  ="__storagekey__"
+	}
+	}
+  provider "azurerm" {
   features {}
 }
 
