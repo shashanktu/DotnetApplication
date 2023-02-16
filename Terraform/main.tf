87562416f6421e01eac1f0578464d68e60a0f3d8
Raj Jammana <Chandrasekhar.Siddabattula@valuemomentum.com>
2023-02-16 15:45:32 +0530

    adding

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,6 +1,6 @@
 terraform {
   required_version = ">= 0.11" 
- backend "azurerm" {
+  backend "azurerm" {
   storage_account_name = "__terraformstorageaccount__"
     container_name       = "terraformakscontainer"
     key                  = "terraform.tfstate"
